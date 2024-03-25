b0ccd461c69a84f627d8d28a941417f9b8531802
Alexandra Snelling <alexandrasnelling@gmail.com>
2024-03-25 01:34:54 -0600

    change slider colour to yellow, calculate MAPE based on selected date range, resize graph and mod background

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.title('Hourly Toronto Electricity Demand Prediction')
 style = """
 <style>
     .stApp {
-        background-image: linear-gradient(to bottom, yellow, green, darkgreen, darkblue);
+        background-image: linear-gradient(to bottom, yellow, darkgreen, darkblue);
         color: #fff;
     }
 </style>
@@ -105,10 +105,19 @@ start_date, end_date = st.select_slider(
     value=(pd.to_datetime(plot_data.index).min().date(), pd.to_datetime(plot_data.index).max().date())
 )
 
+# inject custom CSS for the slider
+st.markdown("""
+<style>
+    .stSlider > div > div > div.css-1hwfws3 {
+        background-color: yellow !important;
+    }
+</style>
+""", unsafe_allow_html=True)
+
 # Filter data based on the selected date range
 plot_data_filtered = plot_data[(plot_data.index >= str(start_date)) & (plot_data.index <= str(end_date))]
 
-fig, ax = plt.subplots(figsize=(24, 6))
+fig, ax = plt.subplots(figsize=(34, 8))  
 # plot_data.plot(ax=ax)
 plot_data_filtered.plot(ax=ax)
 ax.set_xlabel('Date/Time')
@@ -117,5 +126,16 @@ ax.set_title('Hourly Toronto Electricity Demand vs. Predicted Demand')
 ax.legend()
 st.pyplot(fig)
 
-# write with Streamlit
-st.write(f"Mean Absolute Percentage Error: {mape}%")
+
+# # write with Streamlit
+# st.write(f"Mean Absolute Percentage Error: {mape}%")
+
+# Filter the original and predicted dataframes based on selected date range
+actual_filtered = y[(y.index >= str(start_date)) & (y.index <= str(end_date))]
+predictions_filtered = y_predict[(y_predict.index >= str(start_date)) & (y_predict.index <= str(end_date))]
+
+# Recalculate MAPE with filtered data
+mape_filtered = round((abs((actual_filtered - predictions_filtered) / actual_filtered).mean()) * 100, 2)
+
+# Display the updated MAPE
+st.write(f"Filtered Mean Absolute Percentage Error: {mape_filtered}%")
