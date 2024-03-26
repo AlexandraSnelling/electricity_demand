bfa9e299ac376519a502398f132b9290e13708ef
Alexandra Snelling <alexandrasnelling@gmail.com>
2024-03-26 11:42:50 -0600

    attempt to fix adding test Google slides published link to homepage v2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit.components.v1 import components  # Add this line to import components
 from Toronto_2024_Electricity_Demand_Forecast import show_Toronto_2024_Electricity_Demand_Forecast
 from Toronto_2023_Electricity_Demand_Modeling import show_Toronto_2023_Electricity_Demand_Modeling
 
@@ -11,12 +10,35 @@ page = st.sidebar.selectbox("Choose a page", ["Home", "Forecast", "Modeling"])
 
 if page == "Home":
     st.title("This is your home page.")
-    # Place the components.iframe call inside the condition where you want it to appear
-    components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRbaUc-_mCv8y5FlWOnOfxNjGEqej_AaXMBgHDYRyQ2A_3AZ2DSxG_1XKzZQqaU8FG9ptALM0ic3KmK/embed?start=true&loop=true&delayms=3000", height=569)
+    # Use st.markdown to embed the Google Slides iframe
+    st.markdown("""
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRbaUc-_mCv8y5FlWOnOfxNjGEqej_AaXMBgHDYRyQ2A_3AZ2DSxG_1XKzZQqaU8FG9ptALM0ic3KmK/embed?start=true&loop=true&delayms=3000" height="569" width="960" frameborder="0" allowfullscreen="true"></iframe>
+    """, unsafe_allow_html=True)
 elif page == "Forecast":
     show_Toronto_2024_Electricity_Demand_Forecast()
 elif page == "Modeling":
     show_Toronto_2023_Electricity_Demand_Modeling()
+
+
+# import streamlit as st
+# from streamlit.components.v1 import components  # Add this line to import components
+# from Toronto_2024_Electricity_Demand_Forecast import show_Toronto_2024_Electricity_Demand_Forecast
+# from Toronto_2023_Electricity_Demand_Modeling import show_Toronto_2023_Electricity_Demand_Modeling
+
+# # Set the page config here, as the very first Streamlit call
+# st.set_page_config(layout="wide", page_title="Electricity Demand Forecast")
+
+# # Page selection
+# page = st.sidebar.selectbox("Choose a page", ["Home", "Forecast", "Modeling"])
+
+# if page == "Home":
+#     st.title("This is your home page.")
+#     # Place the components.iframe call inside the condition where you want it to appear
+#     components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRbaUc-_mCv8y5FlWOnOfxNjGEqej_AaXMBgHDYRyQ2A_3AZ2DSxG_1XKzZQqaU8FG9ptALM0ic3KmK/embed?start=true&loop=true&delayms=3000", height=569)
+# elif page == "Forecast":
+#     show_Toronto_2024_Electricity_Demand_Forecast()
+# elif page == "Modeling":
+#     show_Toronto_2023_Electricity_Demand_Modeling()
 
     
 # import streamlit as st
